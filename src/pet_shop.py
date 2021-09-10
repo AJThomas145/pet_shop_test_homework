24567d6ed4cac07654af05a923179df1dea42a3a
Andrew Thomas <ajthomas145@gmail.com>
2021-09-10 14:46:16 +0100

    second commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,2 +1,14 @@
-def get_pet_shop_name(heading)
+def get_pet_shop_name(user):
+    return user["name"]
 
+def get_total_cash(user):
+    return user["admin"]["total_cash"]
+
+def add_or_remove_cash(user, amount):
+    user["admin"]["total_cash"] += amount
+
+def get_pets_sold(user):
+    return user["admin"]["pets_sold"]
+
+def increase_pets_sold(user, new_sales):
+    user["admin"]["pets_sold"] += new_sales
