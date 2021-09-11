ccb2678476633acfda75989e0b967c4c0fe7ac36
Andrew Thomas <ajthomas145@gmail.com>
2021-09-11 15:07:22 +0100

    sixth commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,3 +1,4 @@
+import pdb
 def get_pet_shop_name(user):
     return user["name"]
 
@@ -16,9 +17,29 @@ def increase_pets_sold(user, new_sales):
 def get_stock_count(user):
     return len(user["pets"])
 
-def get_pets_by_breed(users, breed):
+def get_pets_by_breed(user, breed):
     number_in_breed = [] 
-    for dog in users["pets"]:
+    for dog in user["pets"]:
         if dog["breed"] == breed:
             number_in_breed.append(dog)
     return number_in_breed
+
+def find_pet_by_name(user, name):
+    for dog in user["pets"]:
+        if dog["name"] == name:
+            return dog
+
+def remove_pet_by_name(user, remove_pet):
+    pet_to_delete = None
+    for dog in user["pets"]:
+        if dog["name"] == remove_pet:
+            pet_to_delete = dog
+            break
+
+    user["pets"].remove(pet_to_delete)
+
+def add_pet_to_stock(user, new_pet):
+    user["pets"].append(new_pet)
+
+
+    
